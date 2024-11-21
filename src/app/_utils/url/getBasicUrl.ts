83048545d1ceacc133c0caacc0de8a34d4293bc1
mrkykvnk <yakovenkomarko@gmail.com>
2024-11-21 02:56:42 +0100

    Added deploy URL handling in .env

--- src/app/_utils/url/getBasicUrl.ts
+++ src/app/_utils/url/getBasicUrl.ts
@@ -1,5 +1,9 @@
 const getBasicUrl = (): string => {
-    const basicUrl = process.env.NEXT_PUBLIC_BASE_URL
+    const isInDev = process.env.NEXT_PUBLIC_DEV
+    const basicDevUrl = process.env.NEXT_PUBLIC_BASE_URL_DEV
+    const basicProductionUrl = process.env.NEXT_PUBLIC_BASE_URL_PRODUCTION
+
+    const basicUrl = isInDev ? basicDevUrl : basicProductionUrl
     if (!basicUrl) throw new Error("Can't read basic URL from .env file")
     return basicUrl
 }
